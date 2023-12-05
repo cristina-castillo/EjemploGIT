f16d76092ae2bb8996d3f2a6586fd5e3132bc5bf
CCM <CCM@C>
2023-12-05 18:48:11 +0100

    Interfaces y modelo

---
+++ Prueba_GIT_distancia/src/main/java/Modelo/MiClase.java
@@ -0,0 +1,28 @@
+package Modelo;
+
+import interfaces.Myinterfaz;
+
+public class MiClase implements Myinterfaz{
+
+	@Override
+	public int metododevuelveentero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+
+
+
+ 
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
