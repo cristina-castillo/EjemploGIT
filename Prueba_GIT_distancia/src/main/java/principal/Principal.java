70cb2d40c88944d38a20d596c99f70468dce39ea
CCM <CCM@C>
2023-12-05 18:56:32 +0100

    Primera semana de trabajo

--- Prueba_GIT_distancia/src/main/java/principal/Principal.java
+++ Prueba_GIT_distancia/src/main/java/principal/Principal.java
@@ -5,6 +5,15 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		System.out.println ("Comienzo mi programa dificilísimo");
+		System.out.println ("Sigo con mi programa dificilísimo");
+		System.out.println ("Añado funcionalidades a mi programa dificilísimo");
+		System.out.println ("Estoy muy contento con mi programa dificilísimo");
+		
+		
+		
+		
 	}
 
 }
